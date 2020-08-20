189f5a77fa52c9c0afc7061728e6303da79b82f8
Peter Nielsen <bregnholm@banbib.dk>
2020-08-20 20:23:16 +0200

    Updates

---
+++ precache-manifest.fd0efe9ca33fead45940062cb26555d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdf2a4b601442d52ceb86ddd31fcae5f",
+    "url": "/walks/index.html"
+  },
+  {
+    "revision": "acecd7478e0d7e5f5495",
+    "url": "/walks/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "9dce74e82071c1650ccc",
+    "url": "/walks/static/css/main.52c22ace.chunk.css"
+  },
+  {
+    "revision": "acecd7478e0d7e5f5495",
+    "url": "/walks/static/js/2.573828e9.chunk.js"
+  },
+  {
+    "revision": "9dce74e82071c1650ccc",
+    "url": "/walks/static/js/main.262f8f28.chunk.js"
+  },
+  {
+    "revision": "b483b341ad7f4e841c9a",
+    "url": "/walks/static/js/runtime-main.d424748d.js"
+  }
+]);
